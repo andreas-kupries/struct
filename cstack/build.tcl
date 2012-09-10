2355fb847892bd030744898904ee027a30f49de2
Andreas Kupries <akupries@shaw.ca>
2012-09-10 02:13:10 +0000

    c::slice, c::stack : Reworked install recipe to allow use of critcl executable, not just package. Also allow specification of a config to build for. Added empty test recipes.

--- cstack/build.tcl
+++ cstack/build.tcl
@@ -119,7 +119,7 @@ proc _recipes {} {
     return
 }
 proc Hinstall {} { return "?destination?\n\tInstall all packages.\n\tdestination = path of package directory, default \[info library\]." }
-proc _install {{ldir {}}} {
+proc _install {{ldir {}} {config {}}} {
     global packages
     if {[llength [info level 0]] < 2} {
 	set ldir [info library]
@@ -140,8 +140,13 @@ proc _install {{ldir {}}} {
 	set src     $::mydir/$p.tcl
 	set version [version $src]
 
-	file delete -force             [pwd]/BUILD
-	critcl::app::main [list -cache [pwd]/BUILD -libdir $ldir -includedir $idir -pkg $src]
+	file delete -force             [pwd]/BUILD.$p
+
+	if {$config ne {}} {
+	    RunCritcl -target $config -cache [pwd]/BUILD.$p -libdir $ldir -includedir $idir -pkg $src
+	} else {
+	    RunCritcl -cache [pwd]/BUILD.$p -libdir $ldir -includedir $idir -pkg $src
+	}
 
 	if {![file exists $ldir/$p]} {
 	    set ::NOTE {warn {DONE, with FAILURES}}
@@ -318,6 +323,10 @@ proc _drop {{dst {}}} {
 	puts "Removed package:     $dstl/$name$version"
     }
 }
+proc Htest {} { return "\n\tRun the package testsuites." }
+proc _test {{config {}}} {
+    puts {No tests available}
+}
 proc Hdoc {} { return "?destination?\n\t(Re)Generate the embedded documentation." }
 proc _doc {{dst {../../embedded/stack}}} {
     cd $::topdir/doc/stack
@@ -369,4 +378,48 @@ proc _figures {} {
 
     return
 }}
+
+proc RunCritcl {args} {
+    #puts [info level 0]
+    if {![catch {
+	package require critcl::app 3.1
+    }]} {
+	#puts "......... [package ifneeded critcl::app [package present critcl::app]]"
+	critcl::app::main $args
+	return
+    } else {
+	foreach cmd {
+	    critcl3 critcl3.kit critcl3.tcl critcl3.exe
+	    critcl critcl.kit critcl.tcl critcl.exe
+	} {
+	    # Locate the candidate.
+	    set cmd [auto_execok $cmd]
+
+	    # Ignore applications which were not found.
+	    if {![llength $cmd]} continue
+
+	    # Proper native path needed, especially on windows. On
+	    # windows this also works (best) with a starpack for
+	    # critcl, instead of a starkit.
+
+	    set cmd [file nativename [lindex [auto_execok $cmd] 0]]
+
+	    # Ignore applications which are too old to support
+	    # -v|--version, or are too old as per their returned
+	    # version.
+	    if {[catch {
+		set v [eval [list exec $cmd --version]]
+	    }] || ([package vcompare $v 3.1] < 0)} continue
+
+	    # Perform the requested action.
+	    set cmd [list exec 2>@ stderr >@ stdout $cmd {*}$args]
+	    #puts "......... $cmd"
+	    eval $cmd
+	    return
+	}
+    }
+
+    puts "Unable to find a usable critcl 3.1 application (package). Stop."
+    ::exit 1
+}
 main
