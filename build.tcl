d1b472f6c593e1180c16dcd873c1e86f68b0ae2a
Andreas Kupries <akupries@shaw.ca>
2012-09-13 18:51:53 +0000

    Modified all build scripts to report and use explicit dependency information. Ensures that requirements of a package P are handled before P itself.

--- build.tcl
+++ build.tcl
@@ -3,29 +3,60 @@
 
 package require Tcl 8.5
 
+set sep _______________________________________________
+set noe [info nameofexecutable]
+
+
+proc get {name args} {
+    global noe
+    if {![catch {
+	exec 2>@ stderr $noe $name/build.tcl {*}$args
+    } result]} {
+	return $result
+    }
+    return {}
+}
+
+proc run {name args} {
+    global noe sep
+    puts "$name $sep $args"
+    if {[catch {
+	exec 2>@ stderr >@ stdout $noe $name/build.tcl {*}$args
+    } msg]} { puts $msg }
+    puts ""
+    puts ""
+    return
+}
+
+proc do {name} {
+    global argv ok
+    # Ignore if already done.
+    if {[dict exists $ok $name]} return
+    # Handle requirements first.
+    foreach r [get $name require] { do $r }
+    # Then ourselves.
+    run $name {*}$argv
+    dict set ok $name .
+}
+
 # Run all build.tcl applications found in sub-directories, passing any
 # arguments.
 #
-# For the moment we use dict sorting and assume that this is good
-# enough to handle the package build dependencies. If it isn't we have
-# to go for a more explict approach.
+# The children are run in arbitrary order (as returned by the OS).
+# Each child C is asked for its requirements, which are then done
+# before C, recursively (implicit topological sorting). Requirement
+# loops are currently not detected and will cause infinite recursion
+# until the stack limit.
 
 apply {{self} {
-    global argv
+    global ok
     set selfdir [file dirname $self]
-    set sep _______________________________________________
-    set noe [info nameofexecutable]
 
+    set ok {}
     puts ""
     foreach child [lsort -dict [glob -directory $selfdir */$self]] {
-	puts "[file join {*}[lrange [file split $child] end-1 end]] $sep"
-
-	catch {
-	    exec 2>@ stderr >@ stdout $noe $child {*}$argv
-	}
-
-	puts ""
-	puts ""
+	set name [lindex [file split $child] end-1]
+	do $name
     }
 }} [file tail [info script]]
 exit
