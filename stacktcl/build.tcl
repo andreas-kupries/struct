703c5853ffda1ee3b426c2ad4c9f125ad2efb299
Andreas Kupries <akupries@shaw.ca>
2012-10-25 23:49:49 +0000

    Fixes to benchmark setup.

--- stacktcl/build.tcl
+++ stacktcl/build.tcl
@@ -1,5 +1,6 @@
 #!/bin/sh
 # -*- tcl -*- \
 exec kettle -f "$0" "${1+$@}"
-kettle testsuite ../tests/stacktcl
+kettle testsuite  ../tests/stacktcl
+kettle benchmarks ../benchmarks/stacktcl
 kettle tcl
