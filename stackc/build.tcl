14b78fbf621dab7b1aae63e3978d69f7f004bbeb
Andreas Kupries <akupries@shaw.ca>
2012-10-26 19:42:12 +0000

    struct::stack/C : Added benchmarks.

--- stackc/build.tcl
+++ stackc/build.tcl
@@ -2,5 +2,6 @@
 # -*- tcl -*- \
 exec kettle -f "$0" "${1+$@}"
 kettle depends-on ../cindex ../cslice ../cstack
-kettle testsuite ../tests/stackc
+kettle testsuite  ../tests/stackc
+kettle benchmarks ../benchmarks/stackc
 kettle critcl3
