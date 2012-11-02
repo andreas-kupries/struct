fd3b68c080637bffe622ead3e5583d79009a71b1
Andreas Kupries <akupries@shaw.ca>
2012-11-02 19:42:55 +0000

    Completed benchmarks for queues.

--- queuec/build.tcl
+++ queuec/build.tcl
@@ -2,5 +2,6 @@
 # -*- tcl -*- \
 exec kettle -f "$0" "${1+$@}"
 kettle depends-on ../cindex ../cslice ../cqueue
-kettle testsuite ../tests/queuec
+kettle testsuite  ../tests/queuec
+kettle benchmarks ../benchmarks/queuec
 kettle critcl3
